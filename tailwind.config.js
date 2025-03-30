1b8f846dcae0715ffb307bbb54ab2dba75401f8e
Niko <nikola.kiryakov@gmail.com>
2025-03-29 20:07:23 -0400

    tailwinds+newpages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          sans: ["Inter", "sans-serif"],
+          mono: ["Fira Code", "monospace"],
+        },
+      },
+    },
+    plugins: [],
+  };
